c4833559a534803dfbc4d3f1ce04f372b5a6de30
ritikkumar27 <rkv2771@gmail.com>
2026-08-06 21:51:31 +0530

    db connection for prisma 7

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -1,9 +1,18 @@
 import { Injectable, OnModuleInit, OnModuleDestroy, Logger } from "@nestjs/common";
 import { PrismaClient } from '@prisma/client';
+import { ConfigService } from "@nestjs/config";
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
     private readonly logger = new Logger(PrismaService.name);
+
+    constructor(configService: ConfigService) {
+        const connectionString = configService.get<string>('DATABASE_URL') || process.env.DATABASE_URL;
+        const adapter = new PrismaPg({ connectionString });
+
+        super({adapter});
+    }
 
     async onModuleInit() {
         await this.$connect();
