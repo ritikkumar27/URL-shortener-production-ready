34993105e432062d8c840056d7e0d7e5925ce8d1
ritikkumar27 <rkv2771@gmail.com>
2026-08-07 01:28:35 +0530

    main.ts -> ...

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ZodValidationPipe } from 'nestjs-zod';
+
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3000);
+  const apiPrefix = configService.get<string>('API_PREFIX', 'api/v1');
+
+  //enabling cors - cross origin resource sharing
+  app.enableCors();
+
+  app.useGlobalPipes(new ZodValidationPipe());
+
+  app.setGlobalPrefix(apiPrefix, {
+    exclude: [':code', 'health'],
+  });
+
+  //swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('URL Shortener API')
+    .setDescription('Production greade URL Shortener and Analytics Platform')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(port);
+
+  logger.log(`Application is running on: http://localhost:${port}`);
+  logger.log(`Swagger Docs available at: http://localhost:${port}/docs`);
+
 }
 bootstrap();
