ca34405378ad80a7e6c76decf4b4afca6cacf0d8
ritikkumar27 <rkv2771@gmail.com>
2026-08-07 18:04:17 +0530

    dispatch analytics asynchronously

--- src/modules/links/redirect.controller.ts
+++ src/modules/links/redirect.controller.ts
@@ -1,17 +1,50 @@
-import { Controller, Get, Param, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Param, Res,Req, HttpStatus } from '@nestjs/common';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
-import type { Response } from 'express';
+import type { Response, Request } from 'express';
 import { LinksService } from './links.service';
+import { AnalyticsService } from '../analytics/analytics.service';
+import { privateDecrypt } from 'crypto';
 
 @ApiTags('Redirect')
 @Controller()
 export class RedirectController {
-  constructor(private readonly linksService: LinksService) {}
+  constructor(
+    private readonly linksService: LinksService,
+    private readonly analyticsService: AnalyticsService,
+  ) {}
 
   @Get(':code')
-  @ApiOperation({ summary: 'Redirect to original URL' })
-  async redirect(@Param('code') code: string, @Res() res: Response) {
+  @ApiOperation({ summary: 'Redirect to original URL with async analytics tracking' })
+  async redirect(
+    @Param('code') code: string,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+
+    // resolbing short code
     const link = await this.linksService.resolveShortCode(code);
+
+    // extracting metadata
+    const ip =
+      (req.headers['cf-connecting-ip'] as string) ||
+      (req.headers['x-forwarded-for'] as string)?.split(',')[0] ||
+      req.ip ||
+      req.socket.remoteAddress ||
+      '127.0.0.1';
+
+    const userAgent = req.headers['user-agent'] || 'Unknown';
+    const referrer = req.headers['referer'] || req.headers['referrer'] as string | undefined;
+
+
+    // dispatch analytics async manner
+    this.analyticsService.trackClick({
+      linkId: link.id,
+      ip,
+      userAgent,
+      referrer,
+      timestamp: new Date().toISOString(),
+    }).catch(() => {}); //prevent unhandled promise rejection
+
     return res.redirect(HttpStatus.FOUND, link.originalUrl); // 302 Found
   }
 }
