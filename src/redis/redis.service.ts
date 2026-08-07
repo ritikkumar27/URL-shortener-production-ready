a7b677c82d7fa21bb7f880b2692469934e89f72d
ritikkumar27 <rkv2771@gmail.com>
2026-08-07 15:05:08 +0530

    redis service : create and configure redis client

---
+++ src/redis/redis.service.ts
@@ -0,0 +1,60 @@
+import { Injectable, OnModuleInit, OnModuleDestroy, Logger } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import Redis from 'ioredis';
+
+export interface CachedLink {
+    id: string;
+    originalUrl: string;
+    isActive: boolean;
+    expiresAt: string | null;
+    passwordHash: string | null;
+}
+
+@Injectable()
+export class RedisService
+implements OnModuleInit, OnModuleDestroy {
+
+    private readonly logger = new Logger(RedisService.name);
+    private client!: Redis;
+
+    private readonly DEFAULT_LINK_TTL = 60 * 60 * 24;
+
+    constructor(private readonly configService: ConfigService) {}
+
+    onModuleInit() {
+        const host = this.configService.get<string>('REDIS_HOST', 'localhost');
+        const port = this.configService.get<number>('REDIS_PORT', 6379);
+        const password = this.configService.get<string>('REDIS_PASSWORD');
+
+        this.client = new Redis({
+            host,
+            port,
+            password: password || undefined,
+            lazyConnect: true,
+            maxRetriesPerRequest: 3,
+            retryStrategy: (times) => Math.min(times * 50, 2000),
+        });
+
+        this.client.on('connect', () => this.logger.log('Connected to Redis'));
+        this.client.on('error', (err) => this.logger.error('Redis Error:', err));
+
+        return this.client.connect();
+    }
+
+    async onModuleDestroy() {
+        if(this.client) {
+            await this.client.quit();
+            this.logger.log('Disconnected from Redis');
+        }
+    }
+
+    //raw client for advanced commands or BullMQssssss
+    getClient(): Redis {
+        return this.client;
+    }
+
+    private formatLinkKey(shortCode: string): string{
+        return `link:code:${shortCode}`;
+    }
+
+}
