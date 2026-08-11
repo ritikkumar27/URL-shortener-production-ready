cf23b063ca62c83eb449b5a3e22228e834dc749a
ritikkumar27 <rkv2771@gmail.com>
2026-08-11 23:20:55 +0530

    auth service: register create user

---
+++ src/modules/auth/dto/auth.service.ts
@@ -0,0 +1,62 @@
+import {
+    Injectable,
+    ConflictException,
+    UnauthorizedException,
+    Logger
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from 'src/database/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { RegisterDto } from './auth.dto';
+import * as argon2 from 'argon2';
+
+
+
+
+
+
+
+@Injectable()
+export class AuthService {
+    private readonly logger = new Logger(AuthService.name);
+
+    constructor(
+        private readonly prisma: PrismaService,
+        private readonly jwtService: JwtService,
+        private readonly configService: ConfigService,
+
+    ) {}
+
+
+    async register(dto: RegisterDto){
+        const existing = await this.prisma.user.findUnique({
+            where: {email: dto.email},
+        });
+
+        if(existing) {
+            throw new ConflictException('An account with this email already exists');
+        }
+
+        const passwordHash = await argon2.hash(dto.password);
+
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                passwordHash,
+                name: dto.name ?? null,
+            },
+
+            select: {
+                id: true,
+                email: true,
+                name: true,
+                role: true,
+                createdAt: true,
+            },
+        });
+
+        
+    }
+}
+
+
