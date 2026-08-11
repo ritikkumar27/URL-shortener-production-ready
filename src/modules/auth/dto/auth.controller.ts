d5d3973edf720336ccc2da1215574c07ebd4a4cf
ritikkumar27 <rkv2771@gmail.com>
2026-08-11 23:36:10 +0530

    login controller

--- src/modules/auth/dto/auth.controller.ts
+++ src/modules/auth/dto/auth.controller.ts
@@ -18,4 +18,14 @@ export class AuthController {
     register(@Body() dto: RegisterDto) {
         return this.authService.register(dto);
     }
+
+    @Public()
+    @Post('login')
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation({ summary: 'Log in with email and password' })
+    login(@Body() dto: LoginDto){
+        return this.authService.login(dto);
+    }
+
+    
 }
