543a22bf48b6cea24390952893debb9f015ea516
Ritik Kumar <rkv2771@gmail.com>
2026-09-02 00:33:06 +0530

    camel case linksService

--- src/modules/links/links.controller.ts
+++ src/modules/links/links.controller.ts
@@ -16,31 +16,31 @@ import { CreateLinkDto, UpdateLinkDto } from './dto/links.dto';
 @ApiTags('Links')
 @Controller('links')
 export class LinksController {
-    constructor (private readonly linksservice: LinksService) {}
+    constructor (private readonly linksService: LinksService) {}
 
     @Post()
     @ApiOperation({summary: 'Create a shortened link'})
     @ApiResponse({status: 201, description: 'Short link successfully creted'})
     create(@Body() dto: CreateLinkDto) {
-        return this.linksservice.create(dto);
+        return this.linksService.create(dto);
     }
 
     @Get(':id')
     @ApiOperation({ summary: 'Get link details by ID' })
     findById(@Param('id') id: string) {
-        return this.linksservice.findById(id);
+        return this.linksService.findById(id);
     }
 
     @Patch(':id')
     @ApiOperation({ summary: 'Update a link' })
     update(@Param('id') id: string, @Body() dto: UpdateLinkDto){
-        return this.linksservice.update(id, dto);
+        return this.linksService.update(id, dto);
     }
 
     @Delete(':id')
     @HttpCode(HttpStatus.NO_CONTENT)
     @ApiOperation({ summary: 'Delete a link' })
     remove(@Param('id') id: string){
-        return this.linksservice.remove(id);
+        return this.linksService.remove(id);
     }
 }
