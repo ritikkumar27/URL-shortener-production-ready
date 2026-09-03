4ae4741bd637682828ac8224fa6487bb2d785358
Ritik Kumar <rkv2771@gmail.com>
2026-09-03 19:50:09 +0530

    redirect controller

--- src/modules/links/links.module.ts
+++ src/modules/links/links.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { LinksController } from "./links.controller";
 import { LinksService } from "./links.service";
+import { RedirectController } from "./redirect.controller";
 
 
 
 @Module({
-    controllers: [LinksController],
+    controllers: [LinksController, RedirectController],
     providers: [LinksService],
     exports: [LinksService],
 })
