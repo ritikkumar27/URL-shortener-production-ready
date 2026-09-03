a0685d5f03e87ad02696e1e91ee0fde8390d9fe1
Ritik Kumar <rkv2771@gmail.com>
2026-09-03 13:26:41 +0530

    setting up redis connection and logger

---
+++ src/modules/redis/redis.service.ts
@@ -0,0 +1,58 @@
+import { Injectable, OnModuleDestroy, OnModuleInit, Logger } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import Redis from 'ioredis';
+
+export interface CachedLink {
+    id: string;
+    originalUrl: string;
+    isActive: boolean;
+    expiresAt: string | null;
+    passwordHash: string | null;
+}
+
+@Injectable()
+export class RedisService implements OnModuleInit, OnModuleDestroy {
+    private readonly logger = new Logger(RedisService.name);
+
+    private client!: Redis;
+    //! means we are telling TS that this property will be initialised before it is used.
+    //it will be initialised on lifecycle hook OnModuleInit
+
+    private readonly DEFAULT_LINK_TTL = 60*24*24;
+
+    constructor(private readonly configService: ConfigService) {}
+
+    onModuleInit() {
+        const host = this.configService.get<string>('REDIS_HOST', 'localhost');
+        const port = this.configService.get<number>('REDIS_PORT', 6379);
+        const password = this.configService.get<string>('REDIS_PASSWORD');
+
+        this.client = new Redis({
+            host: host,
+            port: port,
+            password: password || undefined,
+            lazyConnect: true,
+            maxRetriesPerRequest: 3,
+            retryStrategy: (times) => Math.min(times*50, 2000),
+
+        });
+
+        this.client.on('connect', () => this.logger.log('Connected to Redis'));
+        this.client.on('error', (err) => this.logger.log('Redis error:', err));
+
+        return this.client.connect();
+    }
+
+    async onModuleDestroy() {
+        if (this.client) {
+            await this.client.quit();
+            this.logger.log('Disconnected from Redis');
+        }
+    }
+
+    getClient(): Redis {
+        return this.client;
+    }
+
+
+}
