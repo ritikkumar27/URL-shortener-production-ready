489412deec794b206ccede9781288f5b70be20c4
Ritik Kumar <rkv2771@gmail.com>
2026-09-19 22:40:25 +0530

    jest: links service test: mock defining dependent services

--- src/modules/links/links.service.spec.ts
+++ src/modules/links/links.service.spec.ts
@@ -24,5 +24,36 @@ const mockConfigService = {
     get: jest.fn().mockReturnValue('http://localhost:3000')
 }
 
+describe('LinksService', () => {
+    let service: LinksService;
+    let prisma: typeof mockPrismaService;
+    let redis: typeof mockRedisService;
 
+    beforeEach(async () => {
+        //mini nest js environment specifically for this test
+        
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                LinksService,
+                {provide: PrismaService, useValue: mockPrismaService},
+                {provide: RedisService, useValue: mockRedisService},
+                {provide: ConfigService, useValue: mockConfigService},
+            ],
+        }).compile();
+
+        // grabbing instances
+        service = module.get<LinksService>(LinksService);
+        prisma = module.get(PrismaService);
+        redis = module.get(RedisService);
+
+
+        // clring historuy of mock fxn before every test
+        jest.clearAllMocks();
+    });
+
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    })
+
+})
 
