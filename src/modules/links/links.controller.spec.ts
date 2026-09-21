b971c204a05892a390421b58a02b65766c2375bd
Ritik Kumar <rkv2771@gmail.com>
2026-09-21 23:06:05 +0530

    jest: links controller :

--- src/modules/links/links.controller.spec.ts
+++ src/modules/links/links.controller.spec.ts
@@ -7,3 +7,27 @@ const mockLinksService = {
   create: jest.fn(),
   resolveShortCode: jest.fn(),
 };
+
+
+describe('LinksController', () => {
+    let controller: LinksController;
+  let service: typeof mockLinksService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [LinksController],
+      providers: [
+        {
+          provide: LinksService,
+          useValue: mockLinksService,
+        },
+      ],
+    }).compile();
+    controller = module.get<LinksController>(LinksController);
+    service = module.get(LinksService);
+    
+    jest.clearAllMocks();
+  });
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+})
