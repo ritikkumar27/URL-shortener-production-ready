31556393beb6584a4d3bbc37d5ff660124ffd99a
Ritik Kumar <rkv2771@gmail.com>
2026-09-21 23:06:39 +0530

    jest: links controller :  links create

--- src/modules/links/links.controller.spec.ts
+++ src/modules/links/links.controller.spec.ts
@@ -30,4 +30,19 @@ describe('LinksController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+
+  describe('create()', () => {
+    it('should pass the DTO and UserId to LinksService.create() and return the result', async () => {
+      const createDto = { originalUrl: 'https://github.com' };
+      const mockUserId = 'user-123';
+      
+      const expectedResult = { id: '1', shortCode: 'git', originalUrl: 'https://github.com' };
+      service.create.mockResolvedValue(expectedResult);
+      const result = await controller.create(createDto, mockUserId);
+      expect(result).toEqual(expectedResult);
+      
+      expect(service.create).toHaveBeenCalledWith(createDto, mockUserId);
+    });
+  });
 })
